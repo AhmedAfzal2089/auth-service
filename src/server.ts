417603e5144d2b1d4808dc9c3cdf1b963d52abe6
AhmedAfzal2089 <ameafzal@gmail.com>
2024-10-30 10:26:49 +0500

    test lintstage2

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,9 @@
 function welcome(name: string) {
-    return name
+    console.log('hello')
+    const user = {
+        name: 'ahmed',
+    }
+    const fname = user.name
+    return fname + name
 }
 welcome('Ahmed')
